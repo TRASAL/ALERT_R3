3e57d7d95f0ca13980604ee5b473f1a2cd5a7c4b
Inés Pastor-Marazuela <29770213+InesPM@users.noreply.github.com>
2020-12-03 17:58:11 +0100

    Replacing curve_fit by lmfit

--- scripts/multfreq_drift_rate.py
+++ scripts/multfreq_drift_rate.py
@@ -7,51 +7,66 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 from scipy.optimize import curve_fit
+import scipy as sp
+import lmfit
+from lmfit.models import PowerLawModel, LinearModel
+from lmfit import Model, Minimizer, Parameters
 import tools
 from frbpa.utils import get_phase, get_cycle
 from astropy.time import Time
 
-def powlaw(x, *p):
-    (c, a) = p
-    res = c * x ** (a)
+# ------------------------------------------------------------------------- #
+
+def powlaw(x, amplitude, exponent):
+    amp = amplitude #par['amplitude']
+    k = exponent #par['exponent']
+    res = amp * x ** k
     return res
 
-def linear(x, *p):
-    c = p
-    res = c * x
+def linear(x, slope):
+    res = slope * x
     return res
 
-#colors = ['#577590', '#90be6d', '#f9c74f', '#f3722c', '#f94144']
+def res_linear(params, x, data, sigma):
+    slope = params['slope']
+    model = slope * x
+    res = model - data
+    weighted = np.sqrt(res**2/sigma**2)
+    return weighted
+
+def res_powlaw(params, x, data, sigma):
+    amp = params['amplitude']
+    k = params['exponent']
+    model = amp * x ** k
+    res = model - data
+    weighted = np.sqrt(res**2/sigma**2)
+    return weighted
+
+# ------------------------------------------------------------------------- #
+
 colors = ['#7ecca5', '#feeb9d', '#f67a49', '#9e0142']
 burst_data = pd.read_csv('/home/ines/Documents/projects/R3/arts/arts_r3_properties.csv')
-print(burst_data.columns)
 burst_data[['bary_mjd', 'ncomp', 'drift_rate', 'drift_trust']]
 
 # Drift rate Apertif
-period = 16.29
 drift_rate_list, drift_rate_err, fcen_arts = [], [], []
 fcen_arts_all = []
 
 for ii,burst in burst_data.iterrows():
-    phase = get_phase(burst['detection_mjd'], period, ref_mjd=58369.30)
     #if burst['drift_trust']:
     if burst['ncomp'] == 2:
         drift_rate_list.append(burst['drift_rate'])
-        drift_rate_err.append(0.0)
+        drift_rate_err.append(np.sqrt(np.abs(burst['drift_rate'])))
         fcen_arts.append(burst['fcen'])
     if burst['ncomp'] > 2:
         drift_rate_list.append(burst['drift_rate'])
         drift_rate_err.append(burst['drift_rate_err'])
         fcen_arts.append(burst['fcen'])
 
-print(fcen_arts)
 
 dr_arts = np.mean(drift_rate_list)
 dr_arts_err = np.std(drift_rate_list)
-#fcen_arts = [1437.52,1459.5,1417.97,1422.25,1452.66,1451.64,1288.48,1428.31]
-print(len(fcen_arts), len(drift_rate_list))
-
-print("Apertif", dr_arts, dr_arts_err)
+print("Apertif {:.4f}+-{:.4f} MHz/ms".format(dr_arts, dr_arts_err))
 
 
 # Drift rate CHIME-GBT
@@ -59,7 +74,7 @@ print("Apertif", dr_arts, dr_arts_err)
 dr_chgbt = -4.2
 dr_chgbt_err = 0.4
 fcen_chgbt = 400
-print("CHIME-GBT", dr_chgbt, dr_chgbt_err)
+print("CHIME-GBT {:.4f}+-{:.4f} MHz/ms".format(dr_chgbt, dr_chgbt_err))
 
 
 # Drift rate CHIME
@@ -73,9 +88,11 @@ drift_rate_chime = [
         -44.8424422226505
         ]
 
+drift_rate_chime_err = [np.sqrt(np.abs(dr)) for dr in drift_rate_chime]
+
 dr_chime = np.mean(drift_rate_chime)
 dr_chime_err = np.std(drift_rate_chime)
-print("CHIME", dr_chime, dr_chime_err)
+print("CHIME {:.4f}+-{:.4f} MHz/ms".format(dr_chime, dr_chime_err))
 
 fcen_chime = [
         694.359833147273, 513.724573392185, 649.122616735104,
@@ -86,16 +103,6 @@ fcen_chime = [
         712.51972145998
         ]
 
-# drift_rate_chime = [-3.6660, -8.4000, -6.5665]
-#
-# dr_chime = np.mean(drift_rate_chime)
-# dr_chime_err = np.std(drift_rate_chime)
-# fcen_chime = [449.98, 530.93, 672.05] #600
-
-
-print("Drift rate mean {:.1f} +- {:.1f}".format(dr_chime, dr_chime_err))
-
-# ---------
 # Plotting
 plt.figure(figsize=(8,5))
 plt.style.use('/home/ines/.config/matplotlib/stylelib/paper.mplstyle')
@@ -103,34 +110,80 @@ plt.style.use('/home/ines/.config/matplotlib/stylelib/paper.mplstyle')
 plt.errorbar(fcen_chgbt, dr_chgbt, yerr=dr_chgbt_err, mfc=colors[2],
              marker='^', ls='none', label='CHIME/FRB-GBT',
              mec='k', mew=0.5, ecolor='k')
-plt.errorbar(fcen_chime, drift_rate_chime, yerr=0.0, mfc=colors[1],
+plt.errorbar(fcen_chime, drift_rate_chime, yerr=drift_rate_chime_err, mfc=colors[1],
              marker='s', ls='none', label='CHIME/FRB',
              mec='k', mew=0.5, ecolor='k')
-# plt.plot(fcen_chime, drift_rate_chime, mfc=colors[1],
-#              marker='s', ls='none', label='CHIME/FRB',
-#              mec='k', mew=0.5)
 plt.errorbar(fcen_arts, drift_rate_list, yerr=drift_rate_err,
              mfc=colors[0], marker='o', ls='none', label='Apertif',
              mec='k', mew=0.5, ecolor='k')
 
-# Fitting drift rate to power law
-drifts = [dr_chgbt]
-drifts += drift_rate_chime + drift_rate_list
-freq = [fcen_chgbt] + fcen_chime + fcen_arts
+
+# ------------------------------------------------------------------------- #
+# FITTING MULTI-FREQUENCY DRIFT RATE
+## Fitting drift rate to power law
+drifts = np.array([dr_chgbt] + drift_rate_chime + drift_rate_list)
+drifts_err = np.array([dr_chgbt_err] + drift_rate_chime_err + drift_rate_err)
+freq = np.array([fcen_chgbt] + fcen_chime + fcen_arts)
+freqn = freq - np.mean(freq)
 fval = np.logspace(2,4,num=100)
 
-p0 = [-1,1]
-coeff_pow, var_matrix_pow = curve_fit(powlaw, freq, drifts, p0=p0, bounds=((-np.inf,0), (0,np.inf)))
-err_pow = np.sqrt(np.diag(var_matrix_pow))
-print(coeff_pow, err_pow)
+## Using lmfit.fit
+plmodel = Model(powlaw)
+params = Parameters()
+params.add('amplitude', value=-0.6, max=0.)
+params.add('exponent', value=0.5, min=0.)
+plfit = plmodel.fit(drifts, params, x=freq, weights=1/drifts_err)
+print("\nPOWER LAW FIT\n", plfit.fit_report())
 
-p0=-1
-coeff_lin, var_matrix_lin = curve_fit(linear, freq, drifts, p0=p0)
-err_lin = np.sqrt(np.diag(var_matrix_lin))
-print(coeff_lin, err_lin)
+# Using lmfit.minimize
+# params = Parameters()
+# params.add('amplitude', value=-0.6, max=0.)
+# params.add('exponent', value=0.5, min=0.)
+# plminner = Minimizer(res_powlaw, params, fcn_args=(freq, drifts, drifts_err))
+# plfit = plminner.minimize()
+#
+# out = lmfit.fit_report(plfit.params,sort_pars='True')
+# chisq = sum((plfit.residual)**2) # Calculating the reduced chisq
+# redch = chisq/len(freq) # of the fit
+# print("\nPOWER LAW MINIMIZATION\n", out)
+# print("    redch:", redch)
 
+amp = plfit.params.get("amplitude").value
+amp_err = plfit.params.get("amplitude").stderr
+exp = plfit.params.get("exponent").value
+exp_err = plfit.params.get("exponent").stderr
+coeff_pow = [amp, exp]
+err_pow = [amp_err, exp_err]
 
-# Fitted curve
+## Fitting drift rate to linear model
+
+# Using lmfit.fit
+lmodel = Model(linear)
+params = Parameters()
+params.add('slope', value=-0.02, max=0.)
+lfit = lmodel.fit(drifts, params, x=freq, weights=1/drifts_err)
+slope = lfit.params.get("slope").value
+slope_err = lfit.params.get("slope").stderr
+print("\nLINEAR FIT\n", lfit.fit_report())
+
+# Using lmfit.Minimize
+# params = Parameters()
+# params.add('slope', value=-0.02, max=0.)
+# lminner = Minimizer(res_linear, params, fcn_args=(freq, drifts, drifts_err))
+# lfit = lminner.minimize()
+#
+# out = lmfit.fit_report(lfit.params,sort_pars='True')
+# chisq = sum((lfit.residual)**2) # Calculating the reduced chisq
+# redch = chisq/len(freq) # of the fit
+# slope = lfit.params.get("slope").value
+# slope_err = lfit.params.get("slope").stderr
+# print("\nLINEAR MINIMIZATION\n", out)
+# print("    redch:", redch)
+
+coeff_lin = [slope]
+err_lin = [slope_err]
+
+## Fitted curve
 sep_pow_fit = powlaw(fval, *coeff_pow)
 sep_lin_fit = linear(fval, *coeff_lin)
 
